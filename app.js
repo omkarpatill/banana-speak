a253e0f5399fd1e20261f3315bad0d42d5b14afe
omkarpatill <88880978+omkarpatill@users.noreply.github.com>
2022-12-04 21:45:40 +0530

    feature:added minionspeak server

--- app.js
+++ app.js
@@ -2,9 +2,27 @@ var btnTranslate=document.querySelector("#btn-translate");
 var inputText=document.querySelector("#txt-input");
 var outputDiv=document.querySelector("#output");
 
+var serverUrl="https://api.funtranslations.com/translate/minion.json";
+
+
+function constructUrl(text){
+    return serverUrl+"?text="+ text
+}
+
+function errorHandler(){
+console.log("error occurred", error);
+}
 
 function clickHandler(){
-    outputDiv.innerText="banana"+inputText.value;
+    var input=inputText.value;
+
+    fetch(constructUrl(input))
+    .then(Response=>Response.json())
+    .then(json=>{var translatedText=json.contents.translated
+    outputDiv.innerText=translatedText
+    
+    })
+    .catch(errorHandler);
 
 };
 
